058359d7ca12c50a037ec67ec12af99930366983
kcimino <102926538+kcimino@users.noreply.github.com>
2026-02-18 16:02:43 -0500

    fix: improve color options

--- scripts/defineColors.R
+++ scripts/defineColors.R
@@ -1,6 +1,14 @@
-etddBlue <- "#015a71"
-etddGreen <- ""
-lightStripe <- "grey"
-medStripe <- "cornflowerblue"
+etddBlue <- "#165570"
+etddBlueDark <- "#0d4e6a"
+etddLightBlue <- "#a2dfff"
+etddLighterBlue <- "#bae9ff"
+etddGreen <- "#76A487"
+etddGreenOnWhite <- "#548064"
+etddGreenForUseAsBG <- "#b5e5c6"
+etddGreenExtraLight <- "#ceffdf"
+
+lightStripe <- etddGreenExtraLight
+medStripe <- etddGreenForUseAsBG
 borderColor <- "white"
 colorForTextOnETDDBlue <- "white"
+colorForTextOnTableRows <- "black"
