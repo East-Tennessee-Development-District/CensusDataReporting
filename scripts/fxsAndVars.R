5d6618d38e5a94d47667e7fdc0bf6ee1b561e83d
kcimino <102926538+kcimino@users.noreply.github.com>
2026-04-02 15:46:50 -0400

    qol: move shared fxs and vars to script

---
+++ scripts/fxsAndVars.R
@@ -0,0 +1,268 @@
+if (exists("startrun")) {
+  print(fortunes::fortune())
+} else {
+  source(here::here("scripts","startHere.R"))
+}
+
+
+
+# || Variables
+reset <- FALSE
+
+state <- "tn"
+
+countiesInETDD <- c(
+  "Anderson County",
+  "Blount County",
+  "Campbell County",
+  "Claiborne County",
+  "Cocke County",
+  "Grainger County",
+  "Hamblen County",
+  "Jefferson County",
+  "Knox County",
+  "Loudon County",
+  "Monroe County",
+  "Morgan County",
+  "Roane County",
+  "Scott County",
+  "Sevier County",
+  "Union County"
+)
+
+
+
+municipalitiesInAnderson <- c(
+  "Clinton city",
+  "Norris city",
+  "Oak Ridge city",
+  "Oliver Springs",
+  "Rocky Top"
+)
+municipalitiesInBlount <- c(
+  "Alcoa city",
+  "Friendsville city",
+  "Louisville city",
+  "Maryville city",
+  "Rockford city",
+  "Townsend city"
+  
+)
+municipalitiesInCampbell <- c(
+  "Caryville town",
+  "Jacksboro town",
+  "Jellico city",
+  "La Follette city"
+  
+)
+municipalitiesInClaiborne <- c(
+  "Cumberland Gap town", 
+  "Harrogate city",
+  "New Tazewell town", 
+  "Tazewell town"
+  
+)
+municipalitiesInCocke <- c(
+  "Newport city",
+  "Parrottsville town"
+  
+)
+municipalitiesInGrainger <- c(
+  "Bean Station city",
+  "Blaine city",
+  "Rutledge town"
+  
+)
+municipalitiesInHamblen <- c(
+  "Morristown city"
+)
+municipalitiesInJefferson <- c(
+  "Baneberry city",
+  "Dandridge town",
+  "Jefferson City city",
+  "New Market town",
+  "White Pine town"
+  
+)
+municipalitiesInKnox <- c(
+  "Farragut town",
+  "Knoxville city"
+)
+municipalitiesInLoudon <- c( 
+  "Greenback city",
+  "Loudon town",
+  "Philadelphia city",
+  "Lenoir City city"
+  
+  )
+municipalitiesInMonroe <- c( 
+  "Madisonville city",
+  "Sweetwater",
+  "Vonore",
+  "Tellico Plains"
+
+  )
+municipalitiesInMorgan <- c(
+  "Oakdale",
+  "Sunbright",
+  "Wartburg"
+  
+  )
+municipalitiesInRoane <- c( 
+  "Harriman",
+  "Kingston",
+  "Rockwood city"
+  
+  )
+municipalitiesInScott <- c( 
+  "Huntsville",
+  "Oneida",
+  "Winfield"
+  
+  )
+municipalitiesInSevier <- c(
+  "Pigeon Forge",
+  "Pittman Center",
+  "Gatlinburg",
+  "Sevierville"
+  
+)
+municipalitiesInUnion <- c(
+  "Luttrell",
+  "Maynardville",
+  "Plainview"
+)
+
+
+
+municipalitiesInETDD <- c(
+  municipalitiesInAnderson,
+  municipalitiesInBlount,
+  municipalitiesInCampbell,
+  municipalitiesInClaiborne,
+  municipalitiesInCocke,
+  municipalitiesInGrainger,
+  municipalitiesInHamblen,
+  municipalitiesInJefferson,
+  municipalitiesInKnox,
+  municipalitiesInLoudon,
+  municipalitiesInMonroe,
+  municipalitiesInMorgan,
+  municipalitiesInRoane,
+  municipalitiesInScott,
+  municipalitiesInSevier,
+  municipalitiesInUnion
+)
+
+# tmpdf <- data.frame(NAME=municipalitiesInETDD)
+# tmpdff <- read_csv(here::here("data","interim",str_c("censusData",as.character(2020),"place",".csv")))
+# tmpdffcounty <- read_csv(here::here("data","interim",str_c("censusData",as.character(2020),"county",".csv")))
+# tmpdffcounty |> select(NAME,GEOID) |> distinct() |> 
+#   filter(NAME=="Anderson County, Tennessee")
+# 
+# testingGeoID <- tmpdffcounty |> select(NAME,GEOID) |> distinct() |> 
+#   filter(NAME=="Anderson County, Tennessee") |> pull()
+# 
+# tmpdff |> 
+#   filter(str_detect(GEOID, as.character(testingGeoID))) |> select(GEOID,NAME) |> distinct()
+# 
+# fuzzyjoin::stringdist_join(tmpdf, tmpdff, 
+#                            by = "NAME",
+#                            mode = "left",
+#                            ignore_case = TRUE, 
+#                            method = "jw", 
+#                            max_dist = 99, 
+#                            distance_col = "dist") %>%
+#   group_by(NAME.x) %>%
+#   slice_min(order_by = dist, n = 1) |> 
+#   select(NAME.y, NAME.x, GEOID) |> 
+#   distinct() |> 
+#   print(n=50)
+
+
+censusYears <- c(2010, 2020)
+acsYears <- c(2010, 2020)
+
+
+# || Functions
+
+# ||| Getting
+getCensusData <- function(censusYear, state, vars, fileName, surveyName, geographyLevel){
+  varFromAPI <- load_variables(censusYear, dataset=surveyName, cache=TRUE)
+  APIVars <- 
+    c(
+      varFromAPI %>% 
+        filter(str_detect(name, paste(vars, collapse = "|"))) %>% 
+        select(name) %>% 
+        pull()
+    )
+  get_decennial(
+    geography=geographyLevel,
+    variables = APIVars,
+    year = censusYear,
+    output = "tidy",
+    state = state,
+    cache_table = TRUE,
+    # county = countyName,
+    # key = keyring::key_get("CensusApi"),
+    survey = surveyName,
+    show_call = TRUE
+  ) |> 
+    left_join(varFromAPI, by=c("variable"="name")) |> 
+    mutate(
+      label=str_remove_all(label, "!")
+    ) |> 
+    write_csv(file = fileName)
+}
+
+getACSData <- function(acsYear, state, vars, fileName, surveyName, geographyLevel){
+  varFromAPI <- load_variables(acsYear, dataset=surveyName, cache=TRUE)
+  APIVars <- 
+    c(
+      varFromAPI %>% 
+        filter(str_detect(name, paste(vars, collapse = "|"))) %>% 
+        select(name) %>% 
+        pull()
+    )
+  
+  get_acs(
+    geography=geographyLevel,
+    variables = APIVars,
+    year = acsYear,
+    output = "tidy",
+    state = state,
+    cache_table = TRUE,
+    # county = countyName,
+    # key = keyring::key_get("CensusApi"),
+    survey = surveyName,
+    show_call = TRUE
+  ) |> 
+    left_join(varFromAPI, by=c("variable"="name")) |> 
+    mutate(
+      label=str_remove_all(label, "!")
+    ) |> 
+    write_csv(file = fileName)
+  
+}
+
+# ||| Cleaning
+loadIfExists <- function(fileName){
+  
+  if (file.exists(fileName)) {
+    tmpData <- read_csv(fileName)
+    return(tmpData)
+    
+  } else {
+    source(here::here("scripts", "get.R"))
+    tmpData <- read_csv(fileName)
+    return(tmpData)
+    
+  }
+  
+  
+}
+
+
+
+# || Final
+fxsVarsrun <- TRUE
