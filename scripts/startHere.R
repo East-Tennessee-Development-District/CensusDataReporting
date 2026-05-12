c604401c90cd2be1706cb2e750f4f23dd9b870dd
kcimino <102926538+kcimino@users.noreply.github.com>
2026-05-12 16:14:09 -0400

    feat: add fx to create needed data folders

--- scripts/startHere.R
+++ scripts/startHere.R
@@ -30,6 +30,20 @@ library(gt)
 # This sets a seed so any randomized stuff is consistent. I don't know if I need this, but it's a good habit to just do it at the beginning of projects to be safe (for when you add stuff without thinking)
 set.seed(1552)
 
+# this creates any missing folders
+if (!dir.exists(here::here("data"))){
+  dir.create(here::here("data"))
+}
+if (!dir.exists(here::here("data","raw"))){
+  dir.create(here::here("data","raw"))
+}
+if (!dir.exists(here::here("data","interim"))){
+  dir.create(here::here("data","interim"))
+}
+if (!dir.exists(here::here("data","clean"))){
+  dir.create(here::here("data","clean"))
+}
+
 # This gets the date for saving things later
 CurrentDate <- as.character(Sys.Date())
 startrun <- TRUE
