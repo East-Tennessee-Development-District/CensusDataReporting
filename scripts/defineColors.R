df6cffeb4e7e46d2e56019a1f97c33a69ea97a71
kcimino <102926538+kcimino@users.noreply.github.com>
2026-05-20 10:23:39 -0400

    fix: tidy formatting

--- scripts/defineColors.R
+++ scripts/defineColors.R
@@ -7,6 +7,9 @@ etddGreenOnWhite <- "#548064"
 etddGreenForUseAsBG <- "#b5e5c6"
 etddGreenExtraLight <- "#ceffdf"
 
+barColor <- etddBlue
+barOutlineColor <- "black"
+
 lightStripe <- etddGreenExtraLight
 medStripe <- etddGreenForUseAsBG
 borderColor <- "white"
